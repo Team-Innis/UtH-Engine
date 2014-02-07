7d7f519629d52d48036d05d7935393ff4f750c8d
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-02-07 10:22:47 +0200

    Added uniform set functions

--- include/UtH/Core/Shader.hpp
+++ include/UtH/Core/Shader.hpp
@@ -2,6 +2,7 @@
 #define SHADER_H
 
 #include <UtH/Platform/Enums.hpp>
+#include <UtH/Math/Math.hpp>
 #include <string>
 
 namespace uth
@@ -15,6 +16,20 @@ namespace uth
 		bool LoadShader(ShaderType type, std::string path);
 		bool LinkShader();
 		void Use();
+
+
+
+		//////////////////////////////////////////////////////////////
+		// Uniform set functions
+		void SetUniform(const std::string name, const float x);
+        void SetUniform(const std::string name, const float x, const float y);
+        void SetUniform(const std::string name, const float x, const float y, const float z);
+        void SetUniform(const std::string name, const float x, const float y, const float z, const float w);
+		void SetUniform(const std::string name, const umath::vector2& vector);
+		void SetUniform(const std::string name, const umath::vector3& vector);
+		void SetUniform(const std::string name, const umath::vector4& vector);
+		void SetUniform(const std::string name, const umath::matrix3& matrix);
+		void SetUniform(const std::string name, const umath::matrix4& matrix);
 	private:
 		int program;
 	};
--- src/Core/Shader.cpp
+++ src/Core/Shader.cpp
@@ -32,6 +32,54 @@ void Shader::Use()
 {
 	uthGraphics.bindProgram(program);
 }
+
+
+//////////////////////////////////////////////////////////////
+// Uniform set functions
+void Shader::SetUniform(const std::string name, const float x)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, x);
+}
+void Shader::SetUniform(const std::string name, const float x, const float y)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, x, y);
+}
+void Shader::SetUniform(const std::string name, const float x, const float y, const float z)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, x, y, z);
+}
+void Shader::SetUniform(const std::string name, const float x, const float y, const float z, const float w)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, x, y, z, w);
+}
+void Shader::SetUniform(const std::string name, const umath::vector2& vector)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, vector);
+}
+void Shader::SetUniform(const std::string name, const umath::vector3& vector)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, vector);
+}
+void Shader::SetUniform(const std::string name, const umath::vector4& vector)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, vector);
+}
+void Shader::SetUniform(const std::string name, const umath::matrix3& matrix)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, matrix);
+}
+void Shader::SetUniform(const std::string name, const umath::matrix4& matrix)
+{
+	int location = uthGraphics.getUniformLocation(program, name.c_str());
+	uthGraphics.setUniform(location, matrix);
 }
 
 // Private
