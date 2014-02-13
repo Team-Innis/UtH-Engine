3fb64fc30882f113823efe9fd07d26a573ae8532
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-02-13 13:34:45 +0200

    Added vertexbuffer

---
+++ include/UtH/Core/VertexBuffer.hpp
@@ -0,0 +1,33 @@
+#ifndef VERTEXBUFFER_H
+#define VERTEXBUFFER_H
+
+#include <vector>
+#include <UtH/Math/Vector.hpp>
+#include <UtH/Core/Shader.hpp>
+
+namespace uth
+{
+	class VertexBuffer
+	{
+	public:
+		VertexBuffer();
+		~VertexBuffer();
+
+		void addVertex(const umath::vector3 vertex, const umath::vector2 uv);
+		void addVertices(const std::vector<float>& vertices, const std::vector<float>& uvs);
+		void addIndex(const unsigned int index);
+		//void addIndices(const std::vector<unsigned int>& indices);
+
+		void setVertices(Shader* shader) const;
+
+	private:
+		void init();
+
+		unsigned int buffer;
+
+		int vertexCount;
+		std::vector<float> data;
+		std::vector<unsigned int> indices;
+	};
+}
+#endif
---
+++ src/Core/VertexBuffer.cpp
@@ -0,0 +1,67 @@
+#include <UtH/Core/VertexBuffer.hpp>
+#include <UtH/Platform/Graphics.hpp>
+
+using namespace uth;
+
+VertexBuffer::VertexBuffer()
+	: vertexCount(0)
+{ }
+
+VertexBuffer::~VertexBuffer()
+{
+	data.clear();
+	indices.clear();
+	uthGraphics.deleteBuffers(1, &buffer);
+}
+
+
+// Public
+
+void VertexBuffer::addVertex(const umath::vector3 vertex, const umath::vector2 uv)
+{
+	data.push_back(vertex.x);
+	data.push_back(vertex.y);
+	data.push_back(vertex.z);
+
+	data.push_back(uv.x);
+	data.push_back(uv.y);
+	vertexCount++;
+}
+
+void VertexBuffer::addVertices(const std::vector<float>& vertices, const std::vector<float>& uvs)
+{
+	// TODO
+}
+
+void VertexBuffer::addIndex(const unsigned int index)
+{
+	indices.push_back(index);
+}
+
+//void VertexBuffer::addIndices(const std::vector<unsigned int>& indices)
+//{
+//	// TODO
+//}
+
+void VertexBuffer::setVertices(Shader* shader) const
+{
+	uthGraphics.bindBuffer(BufferType::ARRAY_BUFFER, buffer);
+	uthGraphics.setBufferData(BufferType::ARRAY_BUFFER, data.size(), &data.front(),
+		UsageType::STATIC_DRAW);
+
+	const int posOffset = 2*sizeof(float);
+	const int uvOffset = 3*sizeof(float);
+
+	// Attribute name, number of components, datatype, bytes between elements,
+	// offset of first element in buffer
+	shader->setAttributeData("attrPosition", 3, DataType::FLOAT_TYPE, posOffset, 0);
+	shader->setAttributeData("attrUV", 2, DataType::FLOAT_TYPE, uvOffset, &uvOffset);
+	uthGraphics.bindBuffer(BufferType::ARRAY_BUFFER, 0);
+}
+
+// Private
+
+void VertexBuffer::init()
+{
+	uthGraphics.generateBuffers(1, &buffer);
+}
