3ba8f54cbce770f22d490bf9da288b7a2f26dfb1
Juho Saarinen <juho.saarinen@zeitt.net>
2014-02-14 12:18:47 +0200

    Cleaned Makefile syntax

--- android/jni/Platform.mk
+++ android/jni/Platform.mk
@@ -1,17 +1,25 @@
 LOCAL_PATH := $(call my-dir)
+SRC_PATH := ../../src/
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := engine
-LOCAL_SRC_FILES := ../../src/Math/Matrix3.cpp \
-../../src/Math/Matrix4.cpp \
-../../src/Math/Vector2.cpp \
-../../src/Math/Vector3.cpp \
-../../src/Math/Vector4.cpp \
-../../src/Math/Rectangle.cpp
 LOCAL_STATIC_LIBRARIES := android_native_app_glue engine
 LOCAL_SHARED_LIBRARIES := libopenal
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../include/
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../extlibs/openal/include/
+
+#Math
+LOCAL_SRC_FILES := \
+$(SRC_PATH)Math/Matrix3.cpp \
+$(SRC_PATH)Math/Matrix4.cpp \
+$(SRC_PATH)Math/Vector2.cpp \
+$(SRC_PATH)Math/Vector3.cpp \
+$(SRC_PATH)Math/Vector4.cpp \
+$(SRC_PATH)Math/Rectangle.cpp
+
+#Platform
+LOCAL_SRC_FILES += \
+#$(SRC_PATH)Platform/myfile.cpp
 
 include $(BUILD_STATIC_LIBRARY)
