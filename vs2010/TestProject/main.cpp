1c0034fddfa8a016b0fd0b065cd8246f0851dda8
Juho Saarinen <juho.saarinen@zeitt.net>
2014-02-21 13:19:57 +0200

    Fixed OGLCheck

--- vs2010/TestProject/main.cpp
+++ vs2010/TestProject/main.cpp
@@ -51,7 +51,7 @@ int main()
 		else
 			tex.bind();
 
-     	shader.SetUniform("unifSampler", 0);
+     	shader.SetUniform("unifSampler", -2);
 		buf.setVertices(&shader);
 
         uthGraphics.swapBuffers();
