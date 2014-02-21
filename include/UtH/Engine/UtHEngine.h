c19f87f6c32df6a64f9504f286b1f4e4e0e7f9c8
Eracage <eracage@gmail.com>
2014-02-21 16:18:08 +0200

    shortened main function a bit

--- include/UtH/Engine/UtHEngine.h
+++ include/UtH/Engine/UtHEngine.h
@@ -4,9 +4,11 @@
 
 #include <vector>
 #include <UtH\Platform\Singleton.hpp>
+#include <UtH\Engine\Scene.hpp>
 
 #define theHood uth::UtHEngine::getInstance()
 #define Hood uth::UtHEngine::getInstance()
+#define ö Hood.AutoMainLoop()
 
 namespace uth
 {
