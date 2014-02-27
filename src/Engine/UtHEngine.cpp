3a76dc1cc6b28a0ad03638043c2175b99f483b2f
Eracage <eracage@gmail.com>
2014-02-27 10:04:30 +0200

    cleaned automainloop, exit does not work

--- src/Engine/UtHEngine.cpp
+++ src/Engine/UtHEngine.cpp
@@ -1,40 +1,60 @@
-#include "UtH\Engine\UtHEngine.h"
+#include "UtH/Engine/UtHEngine.h"
 
-#include <UtH\Engine\SceneManager.hpp>
+#include <UtH/Engine/SceneManager.hpp>
+
+#include <UtH/Platform/Graphics.hpp>
+#include <UtH/Platform/Window.hpp> 
 
 using namespace uth;
 
 int UtHEngine::AutoMainLoop()
 {
-	/*while (!(GetAsyncKeyState(VK_ESCAPE) & 0x8000))
+    uth::WindowSettings settings;
+    settings.size = umath::vector2(1600, 900);
+    settings.position = umath::vector2(8, 30);
+	settings.contextVersionMajor = 3;
+	settings.contextVersionMinor = 1;
+    settings.fullScreen = false;
+
+	CreateGameWindow(settings);
+
+	while (m_running)
 	{
-		Update();
+		if(!Update())
+			m_running = false;
 		Draw();
-	}*/
+		m_wndw->processMessages();
+	}
+
+    m_wndw->destroy();
 
 	return 0;
 }
 
 UtHEngine::UtHEngine()
+	: m_running(true)
 {}
 
 UtHEngine::~UtHEngine()
 {}
 
-bool UtHEngine::CreateGameWindow()
+bool UtHEngine::CreateGameWindow(uth::WindowSettings &settings)
 {
+	m_wndw = new Window(settings);
+	uthGraphics.setBlendFunction(true, uth::SRC_ALPHA, uth::ONE_MINUS_SRC_ALPHA);
 	return true;
 }
 bool UtHEngine::Update()
 {
-	UtHSceneM.Update(0.1);
-	//window->throwExeption
-	return true;
+	return Update(m_timer.UpdateDeltaTime());
+}
+bool UtHEngine::Update(double dt)
+{
+	return UtHSceneM.Update(dt);
 }
 void UtHEngine::Draw()
 {
+    m_wndw->clear(0.f, 0.f, 1.f);
 	UtHSceneM.Draw();
-	//window->clear();
-	//ObjectManager->Draw();
-	//SwapBuffers();
+    m_wndw->swapBuffers();
 }
