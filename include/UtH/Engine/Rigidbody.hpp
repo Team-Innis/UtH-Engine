4690749cbac7edfbced0ec020c3aa1e96997f18a
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-03-28 14:31:09 +0200

    Added all kinds of setters and getters

--- include/UtH/Engine/Rigidbody.hpp
+++ include/UtH/Engine/Rigidbody.hpp
@@ -14,6 +14,7 @@ namespace uth
 		COLLIDER_BALL
 	};
 
+	// Conversion from pixels to meters
 	const float PIXELS_PER_METER = 50.f;
 
 	class Rigidbody : public Component
@@ -23,6 +24,7 @@ namespace uth
 		Rigidbody(b2World* world, const COLLIDER_TYPE collider = COLLIDER_BOX,
 			const std::string& name = "Rigidbody");
 		// Create a rigidibody with a bit more control
+		// If creating a ball put diameter in size.x
 		Rigidbody(b2World* world, const COLLIDER_TYPE collider, 
 			const umath::vector2& size, const std::string& name = "Rigidbody");
 
@@ -30,9 +32,56 @@ namespace uth
 
 		void Init();
 		void Update(float dt);
-		// Most other functions will not work untile the object is initialzed
-		// This should at most only take a few frames
-		const bool IsInitialized() const;
+
+		// Functions to add all kinds of forces to the object
+		// Adding force will wake up the object
+
+		// Applies force to the center of the object
+		// Force is usually considered to be in Newtons(N)
+		void ApplyForce(const umath::vector2& force);
+		// Applies force to a point on the object (offset from the center of the object)
+		// This means applying force to the point (0, 0) is the same as center of object
+		// Point is expected to be a pixel offset (not Box2D meter offset)
+		void ApplyForce(const umath::vector2& force, const umath::vector2& point);
+
+		// Applies impulse to the center of the object
+		// Force is usually considered to be in Newton seconds(Ns)
+		void ApplyImpulse(const umath::vector2& impulse);
+		// Applies impulse to a point on the object (offset from the center of the object)
+		void ApplyImpulse(const umath::vector2& impulse, const umath::vector2& point);
+
+		// Applies torque to the object
+		// Force is usually considered to be in Newton metres(Nm)
+		// Positive torque means counter-clockwise rotation
+		void ApplyTorque(const float torque);
+
+		// Sets the raw velocity of the object
+		// Velocity in pixels per second
+		void SetVelocity(const umath::vector2& velocity);
+		const umath::vector2 GetVelocity() const;
+
+		// Sets the size of the object(hitbox) in pixels
+		void SetSize(const umath::vector2& size);
+		// Sets the size of the object(hitbox) in Box2D units(meters)
+		void SetUnitSize(const umath::vector2& size);
+		// Same as above
+		void SetSize(const float radius);
+		void SetUnitSize(const float radius);
+
+		// Returns the size of the object(hitbox) in pixels
+		// Returns as a box size so diameter of a ball can be found in vector.x
+		const umath::vector2 GetSize();
+		// Returns the size of the object(hitbox) in Box2D units(meters)
+		const umath::vector2 GetUnitSize();
+		
+		// Sets the position of the object in pixels
+		void SetPosition(const umath::vector2& position);
+		// Get the position in pixels
+		const umath::vector2 GetPosition();
+		// Set the angle of the object in degrees
+		void SetAngle(const float angle);
+		// Get the angle of the object in degrees
+		const float GetAngle();
 
 		// Sets if the object is active
 		// Inactive objects will unaffetec by all physics
@@ -46,35 +95,16 @@ namespace uth
 		void SetBullet(bool value);
 		const bool IsBullet() const;
 
-		// Functions to add all kinds of forces to the object
-		// Adding force will wake up the object
-
-		// Applies force to the center of the object
-		void ApplyForce(const umath::vector2& force);
-		// Applies force to a point on the object (offset from the center of the object)
-		// This means applying force to the point (0, 0) is the same as center of object
-		// Point is expected to be a pixel offset (not Box2D meter offset)
-		void ApplyForce(const umath::vector2& force, const umath::vector2& point);
-
-		// Applies impulse to the center of the object
-		void ApplyImpulse(const umath::vector2& impulse);
-		// Applies impulse to a point on the object (offset from the center of the object)
-		void ApplyImpulse(const umath::vector2& impulse, const umath::vector2& point);
-
-		// Applies torque to the object
-		// Positive torque means counter-clockwise rotation
-		void ApplyTorque(const float torque);
-
 	private:
 		void defaults();
 		void init();
-
-		bool m_isInitialized;
 
 		Rigidbody();
 
 		b2World* m_world;
 		b2Body* m_body;
+		// For resizing etc...
+		b2FixtureDef m_fixtureDef;
 		COLLIDER_TYPE m_collider;
 		umath::vector2 m_size;
 	};
--- src/Engine/Rigidbody.cpp
+++ src/Engine/Rigidbody.cpp
@@ -5,6 +5,7 @@
 // Helper functions
 ////////////////////////////////////
 b2Vec2 umathToBox2D(const umath::vector2& vec);
+umath::vector2 box2DToUmath(const b2Vec2& vec);
 ////////////////////////////////////
 
 using namespace uth;
@@ -43,28 +44,146 @@ void Rigidbody::Init()
 
 void Rigidbody::Update(float dt)
 {
-	if(!m_isInitialized && parent->transform.GetSize().getLenght() > sqrt(2.0))
-		init();
-
-	if(IsInitialized())
-	{
-		b2Vec2 pos = m_body->GetPosition();
-
-		float angDegrees = -m_body->GetAngle() * 180.f / PI;
+		float angDegrees = GetAngle();
 		parent->transform.SetRotation(angDegrees);
 
+		b2Vec2 pos = m_body->GetPosition();
 		umath::vector2 tpos(pos.x, pos.y);
 		tpos *= PIXELS_PER_METER;
-
-		WriteLog("x: %f, y: %f, angle: %f\n", tpos.x, tpos.y, angDegrees);
 		parent->transform.SetPosition(tpos);
-	}
 }
 
 
-const bool Rigidbody::IsInitialized() const
+// Apply forces
+
+void Rigidbody::ApplyForce(const umath::vector2& force)
 {
-	return m_isInitialized;
+	m_body->ApplyForceToCenter(umathToBox2D(force), true);
+}
+
+void Rigidbody::ApplyForce(const umath::vector2& force, const umath::vector2& point)
+{
+	b2Vec2 p = umathToBox2D(point / PIXELS_PER_METER) + m_body->GetPosition();
+	m_body->ApplyForce(umathToBox2D(force), p, true);
+}
+
+void Rigidbody::ApplyImpulse(const umath::vector2& impulse)
+{
+	m_body->ApplyLinearImpulse(umathToBox2D(impulse), m_body->GetPosition(), true);
+}
+
+void Rigidbody::ApplyImpulse(const umath::vector2& impulse, const umath::vector2& point)
+{
+	b2Vec2 p = umathToBox2D(point / PIXELS_PER_METER) + m_body->GetPosition();
+	m_body->ApplyLinearImpulse(umathToBox2D(impulse), p, true);
+}
+
+void Rigidbody::ApplyTorque(const float torque)
+{
+	m_body->ApplyTorque(-torque, true);
+}
+
+
+// Setters/Getters
+
+void Rigidbody::SetVelocity(const umath::vector2& velocity)
+{
+	m_body->SetLinearVelocity(umathToBox2D(velocity));
+}
+
+const umath::vector2 Rigidbody::GetVelocity() const
+{
+	return box2DToUmath(m_body->GetLinearVelocity());
+}
+
+void Rigidbody::SetSize(const umath::vector2& size)
+{
+	SetUnitSize(size / PIXELS_PER_METER);
+}
+
+void Rigidbody::SetUnitSize(const umath::vector2& size)
+{
+	// This function is kinda ugly but what can you do
+
+	if(m_body->GetFixtureList()->GetType() != b2Shape::e_polygon)
+	{
+		WriteLog("WARNING: Calling SetSize(vec2 size) on a ball. Size not updated\n");
+		return;
+	}
+
+	// Remove original fixture
+	m_body->DestroyFixture(m_body->GetFixtureList());
+
+	//WriteLog("x: %f,y: %f\n", size.x, size.y);
+	
+	b2PolygonShape box;
+	box.SetAsBox(size.x, size.y);
+	m_fixtureDef.shape = &box;
+	m_body->CreateFixture(&m_fixtureDef);
+	m_size = size;
+}
+
+void Rigidbody::SetSize(const float radius)
+{
+	SetUnitSize(radius / PIXELS_PER_METER);
+}
+
+void Rigidbody::SetUnitSize(const float radius)
+{
+	// This might be even more ugly
+
+	if(m_fixtureDef.shape->GetType() != b2Shape::e_circle)
+	{
+		WriteLog("WARNING: Calling SetSize(float radius) on a box. Size not updated\n");
+		return;
+	}
+
+	// Remove original fixture
+	m_body->DestroyFixture(m_body->GetFixtureList());
+
+
+	float curAng = m_body->GetAngle();
+	
+	b2CircleShape circle;
+	circle.m_radius = radius;
+	m_fixtureDef.shape = &circle;
+	m_body->CreateFixture(&m_fixtureDef);
+	m_size.x = radius*2;
+	m_size.y = radius*2;
+}
+
+const umath::vector2 Rigidbody::GetSize()
+{
+	return m_size * PIXELS_PER_METER;
+}
+
+const umath::vector2 Rigidbody::GetUnitSize()
+{
+	return m_size;
+}
+
+void Rigidbody::SetPosition(const umath::vector2& position)
+{
+	b2Vec2 p = umathToBox2D(position / PIXELS_PER_METER);
+	m_body->SetTransform(p, m_body->GetAngle());
+}
+
+const umath::vector2 Rigidbody::GetPosition()
+{
+	b2Vec2 pos = m_body->GetPosition();
+	pos *= PIXELS_PER_METER;
+	return box2DToUmath(pos);
+}
+
+void Rigidbody::SetAngle(const float angle)
+{
+	float ang = -angle * PI / 180.f;
+	m_body->SetTransform(m_body->GetPosition(), ang);
+}
+
+const float Rigidbody::GetAngle()
+{
+	return -m_body->GetAngle() * 180.f / PI;
 }
 
 void Rigidbody::SetActive(bool value)
@@ -88,39 +207,11 @@ const bool Rigidbody::IsBullet() const
 	return m_body->IsBullet();
 }
 
-void Rigidbody::ApplyForce(const umath::vector2& force)
-{
-	m_body->ApplyForceToCenter(umathToBox2D(force), true);
-}
-
-void Rigidbody::ApplyForce(const umath::vector2& force, const umath::vector2& point)
-{
-	b2Vec2 p = umathToBox2D(point / PIXELS_PER_METER) + m_body->GetPosition();
-	m_body->ApplyForce(umathToBox2D(force), p, true);
-}
-
-void Rigidbody::ApplyImpulse(const umath::vector2& impulse)
-{
-	m_body->ApplyLinearImpulse(umathToBox2D(impulse), m_body->GetPosition(), true);
-}
-
-
-void Rigidbody::ApplyImpulse(const umath::vector2& impulse, const umath::vector2& point)
-{
-	b2Vec2 p = umathToBox2D(point / PIXELS_PER_METER) + m_body->GetPosition();
-	m_body->ApplyLinearImpulse(umathToBox2D(impulse), p, true);
-}
-
-void Rigidbody::ApplyTorque(const float torque)
-{
-	m_body->ApplyTorque(-torque, true);
-}
 
 // Private
 
 void Rigidbody::defaults()
 {
-	m_isInitialized = false;
 	m_body = nullptr;
 }
 
@@ -140,6 +231,8 @@ void Rigidbody::init()
 
 	m_size /= PIXELS_PER_METER;
 
+	// TODO: remove pointless userdata
+
 	switch(m_collider)
 	{
 	case COLLIDER_BOX:
@@ -147,12 +240,11 @@ void Rigidbody::init()
 		b2PolygonShape dynamicBox;
 		dynamicBox.SetAsBox(m_size.x/2, m_size.y/2);
 
-		b2FixtureDef fixtureDef;
-		fixtureDef.userData = "Laatikko";
-		fixtureDef.shape = &dynamicBox;
-		fixtureDef.density = 1.0f;
-		fixtureDef.friction = 0.3f;
-		m_body->CreateFixture(&fixtureDef);
+		m_fixtureDef.userData = "Box";
+		m_fixtureDef.shape = &dynamicBox;
+		m_fixtureDef.density = 1.0f;
+		m_fixtureDef.friction = 0.3f;
+		m_body->CreateFixture(&m_fixtureDef);
 		}
 		break;
 	case COLLIDER_BALL:
@@ -160,22 +252,26 @@ void Rigidbody::init()
 		b2CircleShape circleShape;
 		circleShape.m_radius = m_size.x/2;
 
-		b2FixtureDef fixtureDef;
-		fixtureDef.userData = "Pallo";
-		fixtureDef.shape = &circleShape;
-		fixtureDef.density = 1.0f;
-		fixtureDef.friction = 0.3f;
-		m_body->CreateFixture(&fixtureDef);
+		m_fixtureDef.userData = "Ball";
+		m_fixtureDef.shape = &circleShape;
+		m_fixtureDef.density = 1.0f;
+		m_fixtureDef.friction = 0.3f;
+		m_body->CreateFixture(&m_fixtureDef);
 		}
 		break;
 	default:
 		WriteLog("Collider type undefined\nThis is probably bad\n");
 		break;
 	}
-	m_isInitialized = true;
 }
 
 b2Vec2 umathToBox2D(const umath::vector2& vec)
 {
 	return b2Vec2(vec.x, vec.y);
 }
+
+umath::vector2 box2DToUmath(const b2Vec2& vec)
+{
+	return umath::vector2(vec.x, vec.y);
+}
+
