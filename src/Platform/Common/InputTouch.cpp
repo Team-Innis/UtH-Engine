adfdf2b78d57fa2b35f69f7ebfb884a5e2c6e46b
zeitt <juho.saarinen@zeitt.net>
2014-04-10 09:23:47 +0300

    TouchInput for computers.

--- src/Platform/Common/InputTouch.cpp
+++ src/Platform/Common/InputTouch.cpp
@@ -1,1 +1,52 @@
 #include <UtH/Platform/Common/InputTouch.hpp>
+#include <UtH/Platform/Debug.hpp>
+
+using namespace uth;
+
+const TouchInput::TouchUnit& TouchInput::operator[](unsigned int id) const
+{
+	WriteError("Touch not defined on computer.");
+	return ID[id];
+}
+
+const TouchMotion::TouchMotion TouchInput::Motion() const
+{
+	WriteError("Touch not defined on computer.");
+	return ID[0].Motion();
+}
+
+void TouchInput::Update(float dt)
+{
+
+}
+
+
+const TouchMotion::TouchMotion TouchInput::TouchUnit::Motion() const
+{
+	WriteError("Touch not defined on computer.");
+	return m_motion;
+}
+
+const int TouchInput::TouchUnit::GetStartIndex() const
+{
+	WriteError("Touch not defined on computer.");
+	return 0;
+}
+
+const umath::vector2 TouchInput::TouchUnit::GetStartPosition() const
+{
+	WriteError("Touch not defined on computer.");
+	return umath::vector2();
+}
+
+const umath::vector2 TouchInput::TouchUnit::GetPosition() const
+{
+	WriteError("Touch not defined on computer.");
+	return umath::vector2();
+}
+
+const umath::vector2 TouchInput::TouchUnit::GetEndPosition() const
+{
+	WriteError("Touch not defined on computer.");
+	return umath::vector2();
+}
