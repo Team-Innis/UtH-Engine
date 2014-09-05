4ea6857e20671286f809700485fc2a2abc5c2113
Juho Saarinen <juho.saarinen@zeitt.net>
2014-09-05 09:27:12 +0300

    Modified ndk path

--- BuildAndroid.bat
+++ BuildAndroid.bat
@@ -2,7 +2,7 @@
 
 start CopyAssets.bat
 
-set NDK_PROJECT_PATH=D:\Ohjelmointi\UtH-Engine\android
+set NDK_PROJECT_PATH=C:\Users\tti12sjuhos\Desktop\Git\UtH-Engine\android
 
-start D:\Ohjelmointi\ndk\ndk-build.cmd
+start C:\Users\Public\android-ndk-r10\ndk-build.cmd
 exit
