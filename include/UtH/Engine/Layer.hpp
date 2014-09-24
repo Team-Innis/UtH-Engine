9968c85874dab71db5d888d7ed9f1dd379e8bf2b
Esko Haila <eracage@gmail.com>
2014-09-24 10:13:30 +0300

    made layer inherit object

--- include/UtH/Engine/Layer.hpp
+++ include/UtH/Engine/Layer.hpp
@@ -8,46 +8,9 @@
 
 namespace uth
 {
-    class GameObject;
-    class RenderTarget;
-
-	class Layer : private Uncopyable
+	class Layer : public Object
 	{
-
-        friend class Scene;
-
-        GameObject m_gameObject;
-
-	public:
-
-        void Clear();
-
-		int GetLayerId() const;
-
-        void SetActive(const bool active);
-        bool IsActive() const;
-
-		GameObject* AddGameObject(GameObject* gameObject);
-		GameObject* RemoveGameObject(GameObject* gameObject, const bool deleteObject = true);
-        GameObject* GetGameObject(const std::string& name);
-        bool HasGameObject(GameObject* gameObject);
-
-		void Update(float dt);
-		void Draw(RenderTarget& target);
-
-
-        Transform& transform;
-
-    private:
-
-        Layer(const int layerId, const bool adoptObjects = true);
-        ~Layer();
-
-		std::set<GameObject*> m_objects;
-
-		const int layerId;
-        const bool m_adopt;
-        bool m_active;
 	};
 }
+
 #endif
